61ea213803f33a3afe0750fd7f06fbf7069a0c19
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-09-08 20:28:30 +0530

    '08SEP2021'

--- pages/components/Home/HomeView1.js
+++ pages/components/Home/HomeView1.js
@@ -20,32 +20,28 @@ export default function HomeView1(props) {
     var data = props.data;
     return (
         <div className="jl_m_center blog-style-one blog-small-grid">
+            <Link exact href={data.totalurl} passHref={true}>
             <div className="jl_m_center_w jl_radus_e" style={{height:'423px'}}>
             <div className="jl_f_img_bg" style={{backgroundImage: 'url("'+process.env.backendURLMAIN+'/'+data.imagethumb+'")'}} />
             
-           
-
-            <div>
-            <h2>{data.totalurl}</h2>
+          
             <div className="text-box">
                 <span className="jl_f_cat"><a className="post-category-color-text" style={{background: '#eba845'}} >{data.category}</a></span>
-                {/* <Link exact href={data.totalurl} passHref={true}> */}
-
                 <h3 className="text-white"> 
+                {/* <Truncate lines={2} ellipsis={<span> ...</span>}>
+                {data.title}
+                        </Truncate>   */}
                         {truncate(data.title)} ...
                         
                         </h3>
-                {/* </Link> */}
                  <span className="jl_post_meta">
-                <span className="post-date"><i className="jli-pen" /><Moment format="MMMM M, YYYY">{data.createdAt}</Moment></span>
+                    <span className="post-date"><i className="jli-pen" /><Moment format="MMMM M, YYYY">{data.createdAt}</Moment></span>
                     
                 </span>
             </div>
-            </div>
-         
             
-
             </div>
+            </Link>
         </div>
     )
 }
